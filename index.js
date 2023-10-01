e1d0224bcbe98bb6d9044080f17f01eff5a09228
Daria Skoryk <dariaskoryk@gmail.com>
2023-10-01 22:38:30 +0200

    result

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const img = document.querySelector('.dice');
+let cislo = 1;
 
+const changeImg = () => {
+  img.src = `img/side${cislo}.svg`;
+  cislo = (cislo + 1) % 6;
+  if (cislo === 0) {
+    cislo = 1;
+  }
+};
+
+img.addEventListener('click', changeImg);
+document.addEventListener('keydown', changeImg);
